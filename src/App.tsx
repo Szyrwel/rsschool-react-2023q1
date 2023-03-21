beb6fe4233e92563c7d489f9094396c216c817d4
Szyrwel <potato_father@list.ru>
2023-03-21 20:57:39 +0300

    fix: fix src

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import './App.css';
 import { MainPage } from 'pages/main/MainPage';
 import { AboutPage } from 'pages/about/AboutPage';
 import { NotFoundPage } from 'pages/not-found/NotFoundPage';
-import { Layout } from './components/layout/Layout1';
+import { Layout } from './components/layout/Layout';
 
 export class App extends Component {
   render(): JSX.Element {
     return (
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     );
   }
 }
