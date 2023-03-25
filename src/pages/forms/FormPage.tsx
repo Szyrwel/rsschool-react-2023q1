dc33b5584a4b71aaa0d6a1499cfa281b61abad4d
Szyrwel <potato_father@list.ru>
2023-03-25 21:51:47 +0300

    feat: add text input

--- src/pages/forms/FormPage.tsx
+++ src/pages/forms/FormPage.tsx
@@ -1,7 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component, createRef, MouseEvent } from 'react';
+import './form-page.scss';
+
+type StateTypes = {
+  onSubmit: boolean;
+  inputText: string;
+  errors: ErrorsType;
+};
+
+type ErrorsType = {
+  inputText: boolean;
+};
+
+enum ERROR_MESSAGE {
+  inputText = 'Please, enter your name',
+}
 
 export class FormPage extends Component {
+  state: StateTypes = {
+    onSubmit: false,
+    inputText: '',
+    errors: {
+      inputText: false,
+    },
+  };
+
+  inputTextRef: React.RefObject<HTMLInputElement> =
+    createRef<HTMLInputElement>();
+
+  handleChange = (): void => {
+    this.setState({
+      inputText: this.inputTextRef.current?.value,
+    });
+  };
+
+  validationInputText = (e: MouseEvent, value: string) => {
+    e.preventDefault();
+    this.setState({ onSubmit: true });
+    console.log(this.state.inputText);
+
+    if (value.length === 0) {
+      this.setState({ errors: { inputText: true } });
+    } else {
+      this.setState({ errors: { inputText: false } });
+    }
+  };
+
+  // createCard = () => {};
+
   render() {
-    return <div></div>;
+    const { onSubmit, errors, inputText } = this.state;
+
+    return (
+      <form className="form">
+        <label className="label">
+          <p>Name</p>
+          <input
+            ref={this.inputTextRef}
+            placeholder="Name"
+            type="text"
+            value={inputText}
+            onChange={this.handleChange}
+          />
+        </label>
+        {onSubmit && errors.inputText ? (
+          <div className="error">{ERROR_MESSAGE.inputText}</div>
+        ) : (
+          <div className="error"></div>
+        )}
+        <button
+          className="btn__submit"
+          onClick={(e) => this.validationInputText(e, inputText)}
+        >
+          Create card
+        </button>
+      </form>
+    );
   }
 }
