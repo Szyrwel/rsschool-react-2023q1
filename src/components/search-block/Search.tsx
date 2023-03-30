08fb3c68967934e0469bd840eba761e0d5f6b0cf
Szyrwel <potato_father@list.ru>
2023-03-30 11:24:25 +0300

    refactor: rewrite components Search on function with use of useState

--- src/components/search-block/Search.tsx
+++ src/components/search-block/Search.tsx
@@ -1,40 +1,26 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './search.scss';
 
-type InputValueType = {
-  inputValue: string;
-};
+export function Search() {
+  const [inputValue, setInputValue] = useState(
+    localStorage.getItem('inputValue') || ''
+  );
 
-export class Search extends Component {
-  state: InputValueType = {
-    inputValue: localStorage.getItem('inputValue') || '',
-  };
-
-  componentDidMount(): void {
-    this.setState({ inputValue: localStorage.getItem('inputValue') || '' });
+  function handleInputValue(event: ChangeEvent<HTMLInputElement>) {
+    const currentInputValue = event.target.value;
+    localStorage.setItem('inputValue', currentInputValue);
+    setInputValue(currentInputValue);
   }
 
-  componentWillUnmount(): void {
-    this.setState({
-      inputValue: localStorage.setItem('inputValue', this.state.inputValue),
-    });
-  }
-
-  handleInputValue(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ inputValue: event.target.value });
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className="search">
-        <input
-          className="search__input"
-          type="text"
-          placeholder="Search"
-          value={this.state.inputValue}
-          onChange={(event) => this.handleInputValue(event)}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="search">
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Search"
+        value={inputValue}
+        onChange={handleInputValue}
+      />
+    </div>
+  );
 }
