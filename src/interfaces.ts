228357700f77d91bcda32fe85faef92354159126
Szyrwel <potato_father@list.ru>
2023-04-02 21:07:03 +0300

    refactor: change types const BOOKS

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,20 +1,9 @@
-export enum Cover {
-  HARDCOVER = 'Hardcover',
-  PAPERBACK = 'Paperback',
-}
-
 export interface Book {
-  category: string;
-  id: number;
   title: string;
   author: string;
   price: number;
   urlToImages: string[];
   rating: number;
-  isBestSeller: boolean;
-  cover: Cover;
-  description: string;
-  amount: number;
 }
 
 export interface Country {
